b2d70fb68e716e567c676b2cb2856ecb6e508f23
Georges Petrov <georgesm.petrov@gmail.com>
2023-09-07 17:11:51 +0200

    add GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 import { IconContext } from '@react-icons/all-files'
 
@@ -18,6 +19,20 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+
+            <Script
+              src='https://www.googletagmanager.com/gtag/js?id=G-QTN3QP6PKB'
+              strategy='afterInteractive'
+            />
+            <Script id='google-analytics' strategy='afterInteractive'>
+              {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-QTN3QP6PKB');
+        `}
+            </Script>
           </Head>
 
           <body>
